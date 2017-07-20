0c288290e685f6446da0c1e2eaadd898f6a2cda3
缘来是1 <1021805382@qq.com>
2017-07-20 10:39:05 +0800

    15.3Sum
    常见的xSum题目

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -4,53 +4,43 @@
 int cmpfunc(const void * a, const void * b);
 
 int** threeSum(int* nums, int numsSize, int* returnSize) {
-	int L,R;
+	int k;
 	int min,mid,max;
 	int **p;
 	int sum,tmp;
 	int lt,rt;
 	int i = 0;
-	p = (int**)malloc(sizeof(int*)*10);
-	L = min = 0;
-	R = max = numsSize - 1;
-	mid = (min+max)/2;
+	p = (int**)malloc(sizeof(int*)*100);
 	*returnSize = 0;
     qsort(nums,numsSize,sizeof(int),cmpfunc);
-    while( L < R ){
-		sum = nums[L]+nums[R];
-		min = L;
-		max = R;
-		mid = (min+max)/2;
-		while(min<=max){
-			tmp = sum + nums[mid];
-			if( tmp == 0 ){
-				if( mid!=L&&mid!=R ){
-					*(p+i) = (int*)malloc(sizeof(int)*3);
-					p[i][0] = nums[L];
-					p[i][1] = nums[mid];
-					p[i][2] = nums[R];
-					i++;
-					break;
-				}else{
+    for( k = 0; k < numsSize - 2;  k++ ){
+		min = k+1;
+		max = numsSize - 1;
+		while( min < max ){
+			tmp = nums[k]+nums[min]+nums[max];
+			if(tmp == 0 ){
+				*(p+i) = (int*)malloc(sizeof(int)*3);
+				p[i][0] = nums[k];
+				p[i][1] = nums[min];
+				p[i][2] = nums[max];
+				i++;
+				while( nums[min] == nums[min+1] ){
 					min++;
 				}
+				while( nums[max] == nums[max-1]){
+					max--;
+				}
+				min++;max--;
 			}else if( tmp < 0 ){
-				min = mid+1;
+				min++;
 			}else{
-				max = mid-1;
+				max--;
 			}
-			mid = (min+max)/2;
 		}
-		if(sum > 0){
-			rt = R;
-			while(nums[R]==nums[rt])
-				R--;
-		}else{
-			lt = L;
-			while(nums[L]==nums[lt])
-				L++;
+		while( nums[k] == nums[k+1] ){
+			k++;
 		}
-    }
+	}
     *returnSize = i;
     return p;
 }
@@ -64,8 +54,12 @@ int cmpfunc(const void * a, const void * b)
 
 int main()
 {
-	int nums[15] = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+	int nums[6] = {-1,0,1,2,-1,-4};
 	int **p;
 	int a;
-	p = threeSum(nums,15,&a);
+	p = threeSum(nums,6,&a);
+	for( int i = 0; i < a; i++ ){
+		printf("[%d,%d,%d]\n",p[i][0],p[i][1],p[i][2]);
+		
+	}
 }
